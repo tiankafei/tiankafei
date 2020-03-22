12a7e2c977eb43cba631f610ebdfffa67175c061
tiankafei <weishuangshuang89@163.com>
2020-03-22 23:17:14 +0800

    提交“增加红黑树排序方法测试”

--- tiankafei-code-tool/tiankafei-base/src/test/java/cn/tiankafei/base/BaseTest.java
+++ tiankafei-code-tool/tiankafei-base/src/test/java/cn/tiankafei/base/BaseTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author tiankafei
@@ -22,6 +24,21 @@ public class BaseTest {
             Object next = iterator.next();
             System.out.println(next);
         }
+    }
+
+    @Test
+    public void testTreeSet(){
+        Set dataSet = new TreeSet();
+        dataSet.add(1);
+        dataSet.add(11);
+        dataSet.add(3);
+        System.out.println(dataSet);
+        System.out.println("===============");
+        Set strSet = new TreeSet();
+        strSet.add("1");
+        strSet.add("11");
+        strSet.add("3");
+        System.out.println(strSet);
     }
 
 }
