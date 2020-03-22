33499d7a509be25bc963d5c9755fa8e769248a4c
tiankafei <weishuangshuang89@163.com>
2020-03-22 21:47:22 +0800

    提交“提交for循环遍历Iterator的测试方法”

--- tiankafei-code-tool/tiankafei-base/src/test/java/cn/tiankafei/base/BaseTest.java
+++ tiankafei-code-tool/tiankafei-base/src/test/java/cn/tiankafei/base/BaseTest.java
@@ -2,6 +2,10 @@ package cn.tiankafei.base;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author tiankafei
  * @since 1.0
@@ -9,8 +13,15 @@ import org.junit.Test;
 public class BaseTest {
 
     @Test
-    public void test(){
-
+    public void testForIterator(){
+        List dataList = new ArrayList<>();
+        dataList.add(1);
+        dataList.add(3);
+        dataList.add(2);
+        for (Iterator iterator = dataList.listIterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println(next);
+        }
     }
 
 }
