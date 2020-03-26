37a1a8bebddcc1210c68c7d7a1baa3a9a695b15e
tiankafei <weishuangshuang89@163.com>
2020-03-26 20:03:01 +0800

    提交“更新忽略配置”

---
+++ tiankafei-code-python/first/first.py
@@ -0,0 +1,12 @@
+print("-----------------------------------------开始")
+
+# temp = input("不妨猜一下小于现在心里想的是哪个数字\n")
+# guess = int(temp)
+# if guess == 8:
+#     print("aaaaaaaaaaaaaa")
+#     print("aaaaaaaaaaaa")
+# else:
+#     print("0000000000000000")
+# print("游戏结束")
+
+dir(__builtins__)
