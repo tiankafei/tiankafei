b2c6408287ed02d8f1c8725ae5e56800adc687ac
tiankafei <weishuangshuang89@163.com>
2020-04-19 01:03:42 +0800

    提交“更新”

--- tiankafei-code-learn/scala-project/src/main/java/cn/tiankafei/bigdata/hadoop/HadoopReduce.java
+++ tiankafei-code-learn/scala-project/src/main/java/cn/tiankafei/bigdata/hadoop/HadoopReduce.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * @author tiankafei
@@ -25,8 +26,10 @@ public class HadoopReduce extends Reducer<Text, IntWritable, Text, IntWritable> 
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int sum = 0;
-        for (IntWritable intWritable : values) {
-            sum += intWritable.get();
+        Iterator<IntWritable> iterator = values.iterator();
+        if(iterator.hasNext()){
+            IntWritable next = iterator.next();
+            sum += next.get();
         }
         result.set(sum);
         context.write(key, result);
