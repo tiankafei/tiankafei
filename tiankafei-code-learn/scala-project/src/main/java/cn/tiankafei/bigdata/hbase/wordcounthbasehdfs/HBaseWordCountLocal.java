ef3706601637635b51ec237d546d6ae10e6de247
tiankafei <weishuangshuang89@163.com>
2020-04-21 00:02:06 +0800

    提交“去掉多余的引用类”

--- tiankafei-code-learn/scala-project/src/main/java/cn/tiankafei/bigdata/hbase/wordcounthbasehdfs/HBaseWordCountLocal.java
+++ tiankafei-code-learn/scala-project/src/main/java/cn/tiankafei/bigdata/hbase/wordcounthbasehdfs/HBaseWordCountLocal.java
@@ -2,14 +2,11 @@ package cn.tiankafei.bigdata.hbase.wordcounthbasehdfs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 /**
