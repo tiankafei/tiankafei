3b21fb09767539ec1c55dc1ea5d25fc2159d02f4
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-04-28 13:07:13 +0800

    提交“如果想实现Java文件的抽象类，不需要将当前bean实例化的话，可以使用abstract属性”

---
+++ tiankafei-code-learn/spring-project/src/test/java/cn/tiankafei/spring/SpringTest5.java
@@ -0,0 +1,24 @@
+package cn.tiankafei.spring;
+
+import cn.tiankafei.spring.entity.Person001;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public class SpringTest5 {
+
+    /**
+     * 如果想实现Java文件的抽象类，不需要将当前bean实例化的话，可以使用abstract属性
+     */
+    @Test
+    public void test01(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc4.xml");
+        Person001 person001 = (Person001) context.getBean("person2");
+        System.out.println(person001);
+    }
+
+}
