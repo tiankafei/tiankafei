1d31ec80ac7a469b8bd2a9b38917606bf4ec47c3
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-04-28 13:20:01 +0800

    提交“利用实例工厂模式创建bean对象”

--- tiankafei-code-learn/spring-project/src/test/java/cn/tiankafei/spring/SpringTest9.java
+++ tiankafei-code-learn/spring-project/src/test/java/cn/tiankafei/spring/SpringTest9.java
@@ -9,15 +9,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author tiankafei
  * @since 1.0
  **/
-public class SpringTest8 {
+public class SpringTest9 {
 
     /**
-     * 利用静态工厂模式创建bean对象
+     * 利用实例工厂模式创建bean对象
      */
     @Test
     public void test01(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("ioc7.xml");
-        Person001 person001 = (Person001) context.getBean("person5");
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc8.xml");
+        Person001 person001 = (Person001) context.getBean("person6");
         System.out.println(person001);
     }
 
