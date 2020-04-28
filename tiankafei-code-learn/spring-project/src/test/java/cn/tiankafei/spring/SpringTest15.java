7a0c8a9180a0da728080b5b3e15b49fe9f7e1b6d
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-04-28 14:20:21 +0800

    提交“SpEL的使用”

---
+++ tiankafei-code-learn/spring-project/src/test/java/cn/tiankafei/spring/SpringTest15.java
@@ -0,0 +1,28 @@
+package cn.tiankafei.spring;
+
+import cn.tiankafei.spring.entity.Person003;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public class SpringTest15 {
+
+    /**
+     * SpEL的使用
+     */
+    @Test
+    public void test01() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc14.xml");
+        Person003 person4 = (Person003) context.getBean("person4");
+        System.out.println(person4);
+
+        //applicationContext没有close方法，需要使用具体的子类
+        ((ClassPathXmlApplicationContext)context).close();
+    }
+
+
+}
