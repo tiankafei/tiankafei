a3cb49d1199fc857b0c7d3f828a58930bb479a93
tiankafei <weishuangshuang89@163.com>
2020-04-29 00:00:02 +0800

    提交“基于注解的配置”

---
+++ tiankafei-code-learn/springmvc-project/src/main/java/cn/tiankafei/springmvc/controller/HelloController1.java
@@ -0,0 +1,20 @@
+package cn.tiankafei.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController1{
+
+    /*
+    * @RequestMapping就是用来标识此方法用来处理什么请求，其中的/可以取消
+    * 取消后默认也是从当前项目的根目录开始查找，一般在编写的时候看个人习惯
+    * 同时，@RequestMapping也可以用来加在类上，
+    * */
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("msg","hello,SpringMVC！这是基于注解的逻辑处理");
+        return "hello";
+    }
+}
