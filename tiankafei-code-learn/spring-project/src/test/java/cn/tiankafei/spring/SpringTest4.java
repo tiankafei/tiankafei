3e115e00abf81d4d7293c996e7a285ac0325e903
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-04-28 13:05:30 +0800

    提交“继承关系bean的配置”

---
+++ tiankafei-code-learn/spring-project/src/test/java/cn/tiankafei/spring/SpringTest4.java
@@ -0,0 +1,27 @@
+package cn.tiankafei.spring;
+
+import cn.tiankafei.spring.entity.Person001;
+import cn.tiankafei.spring.entity.Person003;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public class SpringTest4 {
+
+    /**
+     * 继承关系bean的配置
+     */
+    @Test
+    public void test01(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc3.xml");
+        Person001 person001 = (Person001) context.getBean("person");
+        System.out.println(person001);
+        Person001 person002 = (Person001) context.getBean("person2");
+        System.out.println(person002);
+    }
+
+}
