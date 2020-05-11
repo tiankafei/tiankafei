0ba0f88d9ec0ba048d83c71630cf1312eecb3a4d
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-05-11 08:48:38 +0800

    提交“提交测试类”

--- tiankafei-code-tool/tiankafei-antlr4/src/test/java/cn/tiankafei/antlr4/Antlr4Test.java
+++ tiankafei-code-tool/tiankafei-antlr4/src/test/java/cn/tiankafei/antlr4/Antlr4Test.java
@@ -1,9 +1,37 @@
 package cn.tiankafei.antlr4;
 
+import org.junit.Test;
+
 /**
  * @Author 魏双双
  * @Date 2020/5/9
  * @Version V1.0
  **/
 public class Antlr4Test {
+
+    @Test
+    public void test01(){
+        int type = 4;
+
+        switch (type) {
+            case 0 :
+                System.out.println(type);
+                break;
+            case 1 :
+                System.out.println(type);
+                break;
+            case 2 :
+                System.out.println(type);
+                break;
+            case 3 :
+            case 4 :
+            case 5 :
+                System.out.println(type);
+                break;
+            default:
+                break;
+        }
+
+    }
+
 }
