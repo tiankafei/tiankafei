a723eb96eda7795a628305ec593f666826724866
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-05-20 14:57:08 +0800

    提交“更新druid数据源的配置”

--- tiankafei-code-learn/springboot-project/src/test/java/cn/tiankafei/springbootproject/SpringbootProjectApplicationTests.java
+++ tiankafei-code-learn/springboot-project/src/test/java/cn/tiankafei/springbootproject/SpringbootProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.tiankafei.springbootproject;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootProjectApplicationTests {
 
-    void contextLoads() {
-    }
-
     @Autowired
     private DataSource dataSource;
 
     @Test
     public void test01(){
         System.out.println(dataSource.getClass());
+
+        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+        System.out.println(druidDataSource.getInitialSize());
+        System.out.println(druidDataSource.getMaxActive());
     }
 
 }
