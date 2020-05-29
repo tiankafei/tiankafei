b339af83a5c5bfd22bd2df3a707d046fda797c4e
tiankafei <weishuangshuang89@163.com>
2020-05-30 00:11:09 +0800

    提交“提交aviator扩展工程”

---
+++ tiankafei-code-tool/tiankafei-aviator-extend/src/main/java/cn/tiankafei/aviator/extend/AviatorExtendUtil.java
@@ -0,0 +1,11 @@
+package cn.tiankafei.aviator.extend;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public abstract class AviatorExtendUtil {
+
+
+
+}
---
+++ tiankafei-code-tool/tiankafei-aviator-extend/src/test/java/cn/tiankafei/aviator/extend/AviatorExtendTest.java
@@ -0,0 +1,16 @@
+package cn.tiankafei.aviator.extend;
+
+import org.junit.Test;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public class AviatorExtendTest {
+
+    @Test
+    public void test01(){
+
+    }
+
+}
