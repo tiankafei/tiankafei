72558b1a9bf5f1223f86f3cddcc7e006207401e9
tiankafei <weishuangshuang89@163.com>
2020-05-29 21:47:11 +0800

    提交“增加asm工程”

---
+++ tiankafei-code-tool/tiankafei-asm/src/main/java/cn/tiankafei/asm/AsmUtil.java
@@ -0,0 +1,11 @@
+package cn.tiankafei.asm;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public abstract class AsmUtil {
+
+
+
+}
---
+++ tiankafei-code-tool/tiankafei-asm/src/test/java/cn/tiankafei/asm/AsmTest.java
@@ -0,0 +1,16 @@
+package cn.tiankafei.asm;
+
+import org.junit.Test;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public class AsmTest {
+
+    @Test
+    public void test01(){
+
+    }
+
+}
