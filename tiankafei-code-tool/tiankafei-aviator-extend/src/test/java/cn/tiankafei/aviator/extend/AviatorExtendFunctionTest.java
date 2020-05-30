89501f8aab3729c94c3bbfb7a28f9fd24b8a4ff2
tiankafei <weishuangshuang89@163.com>
2020-05-30 10:34:59 +0800

    提交“更新测试方法”

--- tiankafei-code-tool/tiankafei-aviator-extend/src/test/java/cn/tiankafei/aviator/extend/AviatorExtendFunctionTest.java
+++ tiankafei-code-tool/tiankafei-aviator-extend/src/test/java/cn/tiankafei/aviator/extend/AviatorExtendFunctionTest.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author tiankafei
  * @since 1.0
@@ -19,23 +22,53 @@ public class AviatorExtendFunctionTest {
 
     @Test
     public void testAnd(){
-        String expression = "and(1+2==3, 2+3==4)";
+        String expression = "and(1+2==3, 2+3==5)";
         Object result = AviatorEvaluator.execute(expression);
         log.info("表达式：{}的执行结果为：{}", expression, result);
 
-        expression = "and(1+2==2)";
+        expression = "and(1+2==3)";
+        result = AviatorEvaluator.execute(expression);
+        log.info("表达式：{}的执行结果为：{}", expression, result);
+
+        expression = "1+2==3 && 2+3==5";
+        result = AviatorEvaluator.execute(expression);
+        log.info("表达式：{}的执行结果为：{}", expression, result);
+
+        expression = "1+2==3";
         result = AviatorEvaluator.execute(expression);
         log.info("表达式：{}的执行结果为：{}", expression, result);
     }
 
     @Test
     public void testOr(){
-        String expression = "or(1+2==3, 2+3==4)";
-        Object result = AviatorEvaluator.execute(expression);
+        String expression = "or(a+b==c, d+e==f)";
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("a", 1);
+        dataMap.put("b", 2);
+        dataMap.put("c", 3);
+        dataMap.put("d", 2);
+        dataMap.put("e", 3);
+        dataMap.put("f", 5);
+        Object result = AviatorEvaluator.execute(expression, dataMap);
         log.info("表达式：{}的执行结果为：{}", expression, result);
 
         expression = "or(1+2==2)";
-        result = AviatorEvaluator.execute(expression);
+        result = AviatorEvaluator.execute(expression, dataMap);
+        log.info("表达式：{}的执行结果为：{}", expression, result);
+
+        expression = "a+b==c || d+e==f";
+        dataMap = new HashMap<>();
+        dataMap.put("a", 1);
+        dataMap.put("b", 2);
+        dataMap.put("c", 3);
+        dataMap.put("d", 2);
+        dataMap.put("e", 3);
+        dataMap.put("f", 4);
+        result = AviatorEvaluator.execute(expression, dataMap);
+        log.info("表达式：{}的执行结果为：{}", expression, result);
+
+        expression = "a+b==c";
+        result = AviatorEvaluator.execute(expression, dataMap);
         log.info("表达式：{}的执行结果为：{}", expression, result);
     }
 
