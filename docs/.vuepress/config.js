275f5486e14963bec402293e3a2fc791d23584ac
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-06-12 12:17:49 +0800

    提交“更新配置”

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'tiankafei - java相关技术栈',
+  description: '自己写的一些东西的记录，包括代码与笔记，jdk最低支持1.8',
+  head: [
+    ['link', { rel: 'icon', href: '/img/logo.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+  ]
+}
