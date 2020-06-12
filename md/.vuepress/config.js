2cbcc70bda0cb6873c6dafd7e7eb4584da59e7d8
tiankafei <weishuangshuang89@163.com>
2020-06-12 22:20:26 +0800

    提交“更新配置”

--- md/.vuepress/config.js
+++ md/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   title: 'tiankafei - java相关技术栈',
   description: '自己写的一些东西的记录，包括代码与笔记，jdk最低支持1.8',
   base: '/',
-  dest: 'docs/.vuepress/dist',
+  dest: 'docs',
   head: [],
   plugins: [],
   themeConfig: {
