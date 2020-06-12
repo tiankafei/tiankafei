22aea26adf94ddfba3276cb6308bad5e4b6bcc81
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-06-12 16:47:38 +0800

    提交“更新”

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,23 +11,24 @@ module.exports = {
     nav: [
       {text: '首页', link: '/'},
       {text: '导航', link: '/guide/'},
-      {text: '架构', link: '/tiankafei-docs-架构/'},
-      {text: '大数据', link: '/tiankafei-docs-大数据/'},
-      {text: 'WEB', link: '/tiankafei-docs-web/'},
-      {text: 'IO', link: '/tiankafei-docs-io/'},
-      {text: '数据结构与算法', link: '/tiankafei-docs-数据结构与算法/'},
-      {text: '云', link: '/tiankafei-docs-云/'},
-      {text: 'linux', link: '/tiankafei-docs-linux/'},
-      {text: '程序员的数学', link: '/tiankafei-docs-程序员的数学/'},
-      {text: '其他', link: '/tiankafei-docs-other/'},
-      {text: '英语', link: '/tiankafei-docs-en/'},
+      {text: '架构', link: '/tiankafei-docs-架构/MySQL调优/'},
+      {text: '大数据', link: '/tiankafei-docs-大数据/centos7安装配置Hadoop/'},
+      {text: 'WEB', link: '/tiankafei-docs-web/spring学习笔记/'},
+      {text: 'IO', link: '/tiankafei-docs-io/IO详情介绍/'},
+      {text: '数据结构与算法', link: '/tiankafei-docs-数据结构与算法/java集合/'},
+      {text: '云', link: '/tiankafei-docs-云/Centos7安装Docker并配置使用/'},
+      {text: 'linux', link: '/tiankafei-docs-linux/centos常用命令/'},
+      {text: '程序员的数学', link: '/tiankafei-docs-程序员的数学/距离度量/'},
+      {text: '其他', link: '/tiankafei-docs-other/git上fork后再更新/'},
+      {text: '英语', link: '/tiankafei-docs-en/词性语法学习/'},
       {
         text: '更多文档',
         items: [
           {text: '词性语法学习', link: '/tiankafei-docs-en/词性语法学习/'},
           {text: '第1阶段单词记忆', link: '/tiankafei-docs-en/第1阶段单词记忆/'},
         ]
-      }
+      },
+      {text: 'GitHub', link: 'https://github.com/tiankafei/tiankafei'},
     ],
     sidebar: {
       '/guide/': [
@@ -44,12 +45,12 @@ module.exports = {
           title: '架构',
           collapsable: false,
           children: [
-            'elasticsearch学习笔记',
             'MySQL调优',
             'mysql主从复制安装配置',
+            '使用amoeba实现mysql读写分离',
             'redis学习笔记',
             'zookeeper学习笔记',
-            '使用amoeba实现mysql读写分离',
+            'elasticsearch学习笔记',
           ]
         }
       ],
@@ -79,13 +80,13 @@ module.exports = {
           title: 'WEB',
           collapsable: false,
           children: [
-            'filter-servlet-inteceptor-controller执行顺序图',
             'spring学习笔记',
             'springMVC学习笔记',
             'mybatis学习笔记',
             'springBoot学习笔记',
             'springCloud学习笔记',
             '特别注意的点',
+            'filter-servlet-inteceptor-controller执行顺序图',
             'sharding-jdbc基于springboot的配置',
             '分布式事务',
             '分布式锁',
@@ -159,10 +160,10 @@ module.exports = {
           title: '程序员的数学',
           collapsable: false,
           children: [
+            '距离度量',
             '导数',
             '概率',
             '概率',
-            '距离度量',
             '逻辑',
             '线性代数',
           ]
