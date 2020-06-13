e3455130b2d48535e92383f1c214e27c5fd2be48
tiankafei <weishuangshuang89@163.com>
2020-06-13 08:59:44 +0800

    提交“更新”

--- md/.vuepress/config.js
+++ md/.vuepress/config.js
@@ -11,14 +11,12 @@ module.exports = {
     nav: [
       {text: '首页', link: '/'},
       {text: '导航', link: '/guide/'},
+      {text: 'Java', link: '/tiankafei-docs-java/Java基础'},
+      {text: 'Spring', link: '/tiankafei-docs-spring/spring学习笔记/'},
       {text: '架构', link: '/tiankafei-docs-架构/MySQL调优/'},
       {text: '大数据', link: '/tiankafei-docs-大数据/centos7安装配置Hadoop/'},
-      {text: 'WEB', link: '/tiankafei-docs-web/spring学习笔记/'},
-      {text: 'IO', link: '/tiankafei-docs-io/IO详情介绍/'},
-      {text: '数据结构与算法', link: '/tiankafei-docs-数据结构与算法/java集合/'},
+      {text: 'Linux', link: '/tiankafei-docs-linux/centos常用命令/'},
       {text: '云', link: '/tiankafei-docs-云/Centos7安装Docker并配置使用/'},
-      {text: 'linux', link: '/tiankafei-docs-linux/centos常用命令/'},
-      {text: '程序员的数学', link: '/tiankafei-docs-程序员的数学/距离度量/'},
       {text: '其他', link: '/tiankafei-docs-other/git上fork后再更新/'},
       {text: '英语', link: '/tiankafei-docs-en/词性语法学习/'},
       {
@@ -40,6 +38,39 @@ module.exports = {
           ]
         }
       ],
+      '/tiankafei-docs-java/': [
+        {
+          title: 'Java',
+          collapsable: false,
+          children: [
+            'Java基础',
+            'java集合',
+            '多线程与高并发-JUC',
+            'IO详情介绍',
+            'Lambda表达式和StreamAPI用法',
+            '设计模式',
+            '树结构',
+            '位运算',
+            'JVM从入门到精通',
+          ]
+        }
+      ],
+      '/tiankafei-docs-spring/': [
+        {
+          title: 'Spring',
+          collapsable: false,
+          children: [
+            'spring学习笔记',
+            'springMVC学习笔记',
+            'mybatis学习笔记',
+            'springBoot学习笔记',
+            'springCloud学习笔记',
+            '特别注意的点',
+            'filter-servlet-inteceptor-controller执行顺序图',
+            'sharding-jdbc基于springboot的配置',
+          ]
+        }
+      ],
       '/tiankafei-docs-架构/': [
         {
           title: '架构',
@@ -51,6 +82,8 @@ module.exports = {
             'redis学习笔记',
             'zookeeper学习笔记',
             'elasticsearch学习笔记',
+            '分布式事务',
+            '分布式锁',
           ]
         }
       ],
@@ -72,69 +105,17 @@ module.exports = {
             'scala学习笔记',
             'centos7安装配置Flink',
             'Kafka学习笔记',
-          ]
-        }
-      ],
-      '/tiankafei-docs-web/': [
-        {
-          title: 'WEB',
-          collapsable: false,
-          children: [
-            'spring学习笔记',
-            'springMVC学习笔记',
-            'mybatis学习笔记',
-            'springBoot学习笔记',
-            'springCloud学习笔记',
-            '特别注意的点',
-            'filter-servlet-inteceptor-controller执行顺序图',
-            'sharding-jdbc基于springboot的配置',
-            '分布式事务',
-            '分布式锁',
-          ]
-        }
-      ],
-      '/tiankafei-docs-io/': [
-        {
-          title: 'IO',
-          collapsable: false,
-          children: [
-            'IO详情介绍',
-          ]
-        }
-      ],
-      '/tiankafei-docs-数据结构与算法/': [
-        {
-          title: '数据结构与算法',
-          collapsable: false,
-          children: [
-            'java集合',
-            'Lambda表达式和StreamAPI用法',
-            '多线程与高并发-JUC',
-            '设计模式',
-            '树结构',
-            '位运算',
-            'JVM从入门到精通',
-          ]
-        }
-      ],
-      '/tiankafei-docs-云/': [
-        {
-          title: '云',
-          collapsable: false,
-          children: [
-            'Centos7安装Docker并配置使用',
-            'Centos7安装Gitlab并汉化',
-            'Centos7安装Jenkins并配置使用',
-            'k8s-nfs文件共享安装及使用',
-            'k8s最新版安装过程',
-            'k8s脚本部署',
-            'k8s常用命令',
+            '距离度量',
+            '导数',
+            '概率',
+            '逻辑',
+            '线性代数',
           ]
         }
       ],
       '/tiankafei-docs-linux/': [
         {
-          title: 'linux',
+          title: 'Linux',
           collapsable: false,
           children: [
             'centos常用命令',
@@ -155,17 +136,18 @@ module.exports = {
           ]
         }
       ],
-      '/tiankafei-docs-程序员的数学/': [
+      '/tiankafei-docs-云/': [
         {
-          title: '程序员的数学',
+          title: '云',
           collapsable: false,
           children: [
-            '距离度量',
-            '导数',
-            '概率',
-            '概率',
-            '逻辑',
-            '线性代数',
+            'Centos7安装Docker并配置使用',
+            'Centos7安装Gitlab并汉化',
+            'Centos7安装Jenkins并配置使用',
+            'k8s-nfs文件共享安装及使用',
+            'k8s最新版安装过程',
+            'k8s脚本部署',
+            'k8s常用命令',
           ]
         }
       ],
@@ -176,7 +158,6 @@ module.exports = {
           children: [
             'git上fork后再更新',
             'git同时连接多个远程仓库',
-            'Java基础',
             '常用正则表达式',
           ]
         }
