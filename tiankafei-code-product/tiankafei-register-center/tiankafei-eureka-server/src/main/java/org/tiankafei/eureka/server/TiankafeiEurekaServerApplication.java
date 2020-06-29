7f3b07f3a35d298cd73afcda8c691bfc485289fb
tiankafei <weishuangshuang89@163.com>
2020-06-29 20:03:24 +0800

    提交“更改包路径”

--- tiankafei-code-product/tiankafei-register-center/tiankafei-eureka-server/src/main/java/org/tiankafei/eureka/server/TiankafeiEurekaServerApplication.java
+++ tiankafei-code-product/tiankafei-register-center/tiankafei-eureka-server/src/main/java/org/tiankafei/eureka/server/TiankafeiEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.tiankafei.eureka.server;
+package org.tiankafei.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
