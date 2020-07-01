0790e70e83e5e5309b2fb38a471b649a6d022234
tiankafei <weishuangshuang89@163.com>
2020-07-01 10:38:51 +0800

    提交“更新”

--- tiankafei-code-product/tiankafei-code-generator/tiankafei-code-generate/src/test/java/org/tiankafei/web/generate/UserGenerator.java
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-generate/src/test/java/org/tiankafei/web/generate/UserGenerator.java
@@ -32,8 +32,7 @@ public class UserGenerator {
                 .setPkIdColumnName("id");
 
         Map<String, String[]> map = new HashMap<>();
-//        map.put("user", new String[]{"sys_user_login"});
-        map.put("user", new String[]{"sys_rule_info", "sys_feature_info"});
+        map.put("user", new String[]{"sys_user_login", "sys_role_info", "sys_feature_info", "sys_user_role", "sys_role_feature"});
 
         String directory = "E:\\gits\\tiankafei\\tiankafei-code-product\\tiankafei-service\\tiankafei-user-service";
         for (String moduleName : map.keySet()) {
