51bc5f9a5fae7670b3e3ec9ed2ecfa786aa619fa
tiankafei <weishuangshuang89@163.com>
2020-07-02 10:50:19 +0800

    提交“删除错误的属性引用”

--- tiankafei-code-product/tiankafei-service/tiankafei-user-service/src/main/java/org/tiankafei/user/param/LoginEntity.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-user-service/src/main/java/org/tiankafei/user/param/LoginEntity.java
@@ -3,8 +3,6 @@ package org.tiankafei.user.param;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.experimental.Accessors;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.tiankafei.user.mapper.LoginMapper;
 
 import java.io.Serializable;
 
@@ -16,9 +14,6 @@ import java.io.Serializable;
 @Accessors(chain = true)
 @ApiModel(value = "用户登录对象", description = "用户登录对象")
 public class LoginEntity implements Serializable {
-
-    @Autowired
-    private LoginMapper loginMapper;
 
 
 
