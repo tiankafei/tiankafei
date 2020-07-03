c37dd4c408b66ae840fa6885a5bcb59a01d78fba
tiankafei <weishuangshuang89@163.com>
2020-07-04 00:49:04 +0800

    提交“更新注释”

--- tiankafei-code-product/tiankafei-service/tiankafei-user-login-service/src/main/java/org/tiankafei/user/login/service/impl/CaptchaServiceImpl.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-user-login-service/src/main/java/org/tiankafei/user/login/service/impl/CaptchaServiceImpl.java
@@ -15,18 +15,36 @@ import javax.servlet.http.HttpServletResponse;
 @Service
 public class CaptchaServiceImpl implements CaptchaService {
 
+    /**
+     * 生成验证码
+     * @param request
+     * @param response
+     * @return
+     * @throws VerificationException
+     */
     @Override
     public Object createCaptcha(HttpServletRequest request, HttpServletResponse response) throws VerificationException {
         boolean finish = HappyCaptcha.require(request, response).build().finish();
         return finish;
     }
 
+    /**
+     * 校验验证码
+     * @param captcha
+     * @param request
+     * @return
+     * @throws VerificationException
+     */
     @Override
     public boolean verifyCaptcha(String captcha, HttpServletRequest request) throws VerificationException {
         boolean flag = HappyCaptcha.verification(request, captcha,true);
         return flag;
     }
 
+    /**
+     * 删除验证码
+     * @param request
+     */
     @Override
     public void removeCaptcha(HttpServletRequest request) {
         HappyCaptcha.remove(request);
