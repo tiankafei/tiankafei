aff90517ee8eb97e04516fd6b23fbd754f1c8cdf
tiankafei <weishuangshuang89@163.com>
2020-07-03 12:22:03 +0800

    提交“更新zuul配置”

--- tiankafei-code-product/tiankafei-api-gateway/tiankafei-zuul/src/main/java/org/tiankafei/zuul/filter/AuthFilter.java
+++ tiankafei-code-product/tiankafei-api-gateway/tiankafei-zuul/src/main/java/org/tiankafei/zuul/filter/AuthFilter.java
@@ -1,11 +1,43 @@
 package org.tiankafei.zuul.filter;
 
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+import org.tiankafei.web.common.utils.CommonUtil;
+
+import java.util.List;
+
 /**
  * @author tiankafei
  * @since 1.0
  **/
-public class AuthFilter {
+@Slf4j
+@Component
+public class AuthFilter extends ZuulFilter {
 
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
 
+    @Override
+    public Object run() throws ZuulException {
 
+        return null;
+    }
+
+    @Override
+    protected boolean checkShouldFilter() {
+        List<String> authUrls = exclusionsUrlsProperties.getAuthUrls();
+        boolean flag = CommonUtil.checkUrlStartsWith(authUrls, path);
+
+        if(flag){
+            log.info("不需要鉴权的url：{}", path);
+        }else{
+            log.info("需要鉴权的url：{}", path);
+        }
+
+        return !flag;
+    }
 }
