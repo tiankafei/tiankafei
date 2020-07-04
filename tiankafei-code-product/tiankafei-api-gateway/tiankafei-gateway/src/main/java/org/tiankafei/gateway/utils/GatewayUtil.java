34e3a8b914a9a2a77cfe736b572a14267299a831
tiankafei <weishuangshuang89@163.com>
2020-07-04 23:00:40 +0800

    提交“更新”

--- tiankafei-code-product/tiankafei-api-gateway/tiankafei-gateway/src/main/java/org/tiankafei/gateway/utils/GatewayUtil.java
+++ tiankafei-code-product/tiankafei-api-gateway/tiankafei-gateway/src/main/java/org/tiankafei/gateway/utils/GatewayUtil.java
@@ -22,32 +22,11 @@ public abstract class GatewayUtil {
      * @return
      */
     public static Mono<Void> returnValue(ApiResult apiResult, ServerWebExchange exchange){
-        // 自定义返回结果
-        byte[] bits = JSON.toJSONString(apiResult).getBytes(StandardCharsets.UTF_8);
-        return returnValue(bits, exchange);
-    }
-
-    /**
-     * 返回值转换
-     * @param object
-     * @param exchange
-     * @return
-     */
-    public static Mono<Void> returnValue(Object object, ServerWebExchange exchange){
-        // 自定义返回结果
-        byte[] bits = JSON.toJSONString(object).getBytes(StandardCharsets.UTF_8);
-        return returnValue(bits, exchange);
-    }
-
-    /**
-     * 返回值
-     * @param bits
-     * @param exchange
-     * @return
-     */
-    private static Mono<Void> returnValue(byte[] bits, ServerWebExchange exchange){
         ServerHttpResponse response = exchange.getResponse();
-        DataBuffer buffer = response.bufferFactory().wrap(bits);
+        // 自定义返回结果
+        String returnBody = JSON.toJSONString(apiResult);
+        byte[] byteArray = returnBody.getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = response.bufferFactory().wrap(byteArray);
         return response.writeWith(Mono.just(buffer));
     }
 
