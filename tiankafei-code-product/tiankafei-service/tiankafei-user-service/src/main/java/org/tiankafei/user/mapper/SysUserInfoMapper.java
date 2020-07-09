f9354d358442a597dab016fdcbdabd28ed7a6134
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-07-09 15:52:27 +0800

    提交“删除不用的方法”

--- tiankafei-code-product/tiankafei-service/tiankafei-user-service/src/main/java/org/tiankafei/user/mapper/SysUserInfoMapper.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-user-service/src/main/java/org/tiankafei/user/mapper/SysUserInfoMapper.java
@@ -49,12 +49,4 @@ public interface SysUserInfoMapper extends BaseMapper<SysUserInfoEntity> {
      */
     List<SysUserInfoQueryVo> getSysUserInfoList(@Param("param") SysUserInfoQueryParam sysUserInfoQueryParam);
 
-    /**
-     * 根据用户ID获取用户和角色的对应关系
-     *
-     * @param userId
-     * @return
-     */
-    SysUserInfoQueryVo getSysUserInfoQueryVo(@Param("param") Serializable userId);
-
 }
