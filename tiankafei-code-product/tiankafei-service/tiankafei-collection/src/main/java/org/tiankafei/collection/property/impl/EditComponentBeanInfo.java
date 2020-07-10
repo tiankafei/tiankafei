ea777565ee20d2ecc58d745363f2c12d60b251c9
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-07-10 08:41:52 +0800

    提交“更新属性”

--- tiankafei-code-product/tiankafei-service/tiankafei-collection/src/main/java/org/tiankafei/collection/property/impl/EditComponentBeanInfo.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-collection/src/main/java/org/tiankafei/collection/property/impl/EditComponentBeanInfo.java
@@ -1,7 +1,8 @@
 package org.tiankafei.collection.property.impl;
 
+import java.util.List;
 import org.tiankafei.collection.enums.ComponentTypeEnum;
-import org.tiankafei.collection.property.CheckBoxComponentProperty;
+import org.tiankafei.collection.param.RegularExpressionVo;
 import org.tiankafei.collection.property.EditComponentProperty;
 
 /**
@@ -11,6 +12,67 @@ import org.tiankafei.collection.property.EditComponentProperty;
  * @since 1.0
  */
 public class EditComponentBeanInfo extends BaseComponentProperty implements EditComponentProperty {
+
+    /**
+     * 字段名
+     */
+    protected String fieldName;
+
+    /**
+     * 标签名
+     */
+    protected String labelName;
+
+    /**
+     * 占位提示
+     */
+    protected String placeholder;
+
+    /**
+     * 表单栅格
+     */
+    protected Integer formGrid;
+
+    /**
+     * 标签宽度
+     */
+    protected Integer labelWidth;
+
+    /**
+     * 组件宽度
+     */
+    protected Double componentWidth;
+
+    /**
+     * 默认值
+     */
+    protected Object defaultValue;
+
+    /**
+     * 组件高度
+     */
+    protected Integer componentHeight;
+
+    /**
+     * 是否显示标签
+     */
+    protected Boolean showLabel;
+
+    /**
+     * 水印，可以是字符串，也可以是logo图片的地址
+     */
+    protected String watermark;
+
+    /**
+     * 是否必填
+     */
+    protected Boolean required;
+
+    /**
+     * 正则表达式集合
+     */
+    protected List<RegularExpressionVo> regularExpressionVoList;
+
     @Override
     public Integer getComponentType() {
         return ComponentTypeEnum.EDIT.getCode();
--- tiankafei-code-product/tiankafei-service/tiankafei-collection/src/main/java/org/tiankafei/collection/property/impl/SelectionComponentBeanInfo.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-collection/src/main/java/org/tiankafei/collection/property/impl/SelectionComponentBeanInfo.java
@@ -1,6 +1,9 @@
 package org.tiankafei.collection.property.impl;
 
+import java.util.List;
+import org.tiankafei.base.base.model.CodeNameVo;
 import org.tiankafei.collection.enums.ComponentTypeEnum;
+import org.tiankafei.collection.param.RegularExpressionVo;
 import org.tiankafei.collection.property.SelectionComponentProperty;
 
 /**
@@ -10,6 +13,82 @@ import org.tiankafei.collection.property.SelectionComponentProperty;
  * @since 1.0
  */
 public class SelectionComponentBeanInfo extends BaseComponentProperty implements SelectionComponentProperty {
+
+    /**
+     * 字段名
+     */
+    protected String fieldName;
+
+    /**
+     * 标签名
+     */
+    protected String labelName;
+
+    /**
+     * 占位提示
+     */
+    protected String placeholder;
+
+    /**
+     * 表单栅格
+     */
+    protected Integer formGrid;
+
+    /**
+     * 标签宽度
+     */
+    protected Integer labelWidth;
+
+    /**
+     * 组件宽度
+     */
+    protected Double componentWidth;
+
+    /**
+     * 默认值
+     */
+    protected Object defaultValue;
+
+    /**
+     * 是否显示标签
+     */
+    protected Boolean showLabel;
+
+    /**
+     * 能否清空
+     */
+    protected Boolean clearable;
+
+    /**
+     * 是否禁用
+     */
+    protected Boolean disabled;
+
+    /**
+     * 是否可以搜索
+     */
+    protected Boolean searchabled;
+
+    /**
+     * 是否可以多选
+     */
+    protected Boolean multipleChoice;
+
+    /**
+     * 是否必填
+     */
+    protected Boolean required;
+
+    /**
+     * 选项列表
+     */
+    protected List<CodeNameVo> codeNameList;
+
+    /**
+     * 正则表达式集合
+     */
+    protected List<RegularExpressionVo> regularExpressionVoList;
+
     @Override
     public Integer getComponentType() {
         return ComponentTypeEnum.SELECTION.getCode();
