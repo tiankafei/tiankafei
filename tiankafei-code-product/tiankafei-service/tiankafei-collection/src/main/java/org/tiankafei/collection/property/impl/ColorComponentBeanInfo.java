3bb3d568f4c1c903e18a1838b74cdd0d4f6cff1e
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-07-10 20:04:00 +0800

    提交“更新属性”

--- tiankafei-code-product/tiankafei-service/tiankafei-collection/src/main/java/org/tiankafei/collection/property/impl/ColorComponentBeanInfo.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-collection/src/main/java/org/tiankafei/collection/property/impl/ColorComponentBeanInfo.java
@@ -10,6 +10,82 @@ import org.tiankafei.collection.property.ColorComponentProperty;
  * @since 1.0
  */
 public class ColorComponentBeanInfo extends BaseComponentProperty implements ColorComponentProperty {
+
+    /**
+     * 字段名
+     */
+    protected String fieldName;
+
+    /**
+     * 标签名
+     */
+    protected String labelName;
+
+    /**
+     * 表单栅格
+     */
+    protected Integer formGrid;
+
+    /**
+     * 标签宽度
+     */
+    protected Integer labelWidth;
+
+    /**
+     * 组件宽度
+     */
+    protected Double componentWidth;
+
+    /**
+     * 默认值
+     */
+    protected Object defaultValue;
+
+    /**
+     * 颜色格式
+     */
+    protected String colorFormat;
+
+    /**
+     * 最大值
+     */
+    protected Integer maxValue;
+
+    /**
+     * 是否显示标签
+     */
+    protected Boolean showLabel;
+
+    /**
+     * 允许半选
+     */
+    protected Boolean showHalfSelect;
+
+    /**
+     * 辅助文字
+     */
+    protected String helpText;
+
+    /**
+     * 是否显示分数
+     */
+    protected Boolean showScore;
+
+    /**
+     * 能否清空
+     */
+    protected Boolean clearable;
+
+    /**
+     * 是否禁用
+     */
+    protected Boolean disabled;
+
+    /**
+     * 是否必填
+     */
+    protected Boolean required;
+
     @Override
     public Integer getComponentType() {
         return ComponentTypeEnum.COLOR.getCode();
