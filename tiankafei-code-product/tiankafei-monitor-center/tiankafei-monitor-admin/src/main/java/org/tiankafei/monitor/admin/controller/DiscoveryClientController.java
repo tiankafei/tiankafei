d370b31f610e3151b3a8751d9d201e08b15158f4
tiankafei <weishuangshuang89@163.com>
2020-07-12 02:11:15 +0800

    提交“提交测试url”

---
+++ tiankafei-code-product/tiankafei-monitor-center/tiankafei-monitor-admin/src/main/java/org/tiankafei/monitor/admin/controller/DiscoveryClientController.java
@@ -0,0 +1,27 @@
+package org.tiankafei.monitor.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ */
+@RestController
+public class DiscoveryClientController {
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @GetMapping("/get1")
+    public List<String> get1(){
+        List<String> services = client.getServices();
+        return services;
+    }
+
+
+}
