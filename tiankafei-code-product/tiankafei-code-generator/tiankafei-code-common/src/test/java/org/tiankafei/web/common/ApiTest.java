fd609b54cf1a99134de26f6e435d9c1437e27a41
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-07-15 12:44:34 +0800

    提交“更新依赖及配置”

--- tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/test/java/org/tiankafei/web/common/ApiTest.java
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/test/java/org/tiankafei/web/common/ApiTest.java
@@ -1,6 +1,10 @@
 package org.tiankafei.web.common;
 
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
 import com.alibaba.fastjson.JSON;
+import java.util.List;
 import org.junit.Test;
 import org.tiankafei.web.common.api.ApiResult;
 
@@ -16,6 +20,19 @@ public class ApiTest {
         System.out.println(JSON.toJSONString(ok));
         ApiResult ok1 = ApiResult.ok();
         System.out.println(JSON.toJSONString(ok1));
+    }
+
+    @Test
+    public void test02(){
+        String filePath = "D:\\test\\x-spreadsheet功能清单.xlsx";
+        ExcelReader reader = ExcelUtil.getReader(ResourceUtil.getStream(filePath));
+        List<List<Object>> read = reader.read();
+        read.stream().forEachOrdered(dataList -> {
+            dataList.stream().forEachOrdered(data -> {
+                System.out.print(data + ",");
+            });
+            System.out.println();
+        });
     }
 
 }
