56ec6815799ec4fcfb81c357a158f3e8c75e5614
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-07-15 17:35:38 +0800

    提交“更新链路调用，及注册中心动态更新配置”

---
+++ tiankafei-code-product/tiankafei-service/tiankafei-user-service/tiankafei-user-server/src/main/java/org/tiankafei/user/controller/LoginController.java
@@ -0,0 +1,62 @@
+package org.tiankafei.user.controller;
+
+import cn.hutool.core.util.IdUtil;
+import com.alibaba.fastjson.JSON;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.tiankafei.user.login.feign.LoginFeign;
+import org.tiankafei.user.param.LoginParamVo;
+import org.tiankafei.user.properties.UserProperties;
+import org.tiankafei.web.common.api.ApiResult;
+import org.tiankafei.web.common.controller.BaseController;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+@Slf4j
+@RestController
+@Api(value = "用户登录相关 API", tags = "用户登录相关接口")
+@RefreshScope
+public class LoginController extends BaseController {
+
+    @Autowired
+    private LoginFeign loginFeign;
+
+    @Autowired
+    private UserProperties userProperties;
+
+    @GetMapping("get1")
+    public String get1(){
+        return JSON.toJSONString(userProperties);
+    }
+
+    /**
+     * 用户登录
+     */
+    @GetMapping("/login")
+    @ApiOperation(value = "用户登录", notes = "用户登录")
+    public ApiResult<String> login() throws Exception {
+        LoginParamVo loginParamVo = new LoginParamVo();
+        loginParamVo.setKeywords("tiankafei").setPassword("tiankafei").setUuid(IdUtil.simpleUUID()).setVerificationCode("123");
+        String data = loginFeign.login(loginParamVo).getData();
+        return ApiResult.ok(data);
+    }
+
+    /**
+     * 用户注销
+     */
+    @GetMapping("/logout/{userId}")
+    @ApiOperation(value = "用户注销", notes = "用户注销")
+    public ApiResult<Boolean> logout(@PathVariable String userId) throws Exception {
+        Boolean flag = loginFeign.logout(userId).getData();
+        return ApiResult.ok(flag);
+    }
+
+}
