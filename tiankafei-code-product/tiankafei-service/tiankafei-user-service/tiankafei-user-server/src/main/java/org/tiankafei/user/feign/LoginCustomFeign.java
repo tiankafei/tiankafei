3d26804f3b0df43dd966da270b5ba23d883af641
tiankafei <weishuangshuang89@163.com>
2020-07-21 20:01:44 +0800

    提交“跟新自定义feign的注解属性值”

--- tiankafei-code-product/tiankafei-service/tiankafei-user-service/tiankafei-user-server/src/main/java/org/tiankafei/user/feign/LoginCustomFeign.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-user-service/tiankafei-user-server/src/main/java/org/tiankafei/user/feign/LoginCustomFeign.java
@@ -7,6 +7,6 @@ import org.tiankafei.user.login.feign.LoginFeign;
  * @author 魏双双
  * @since 1.0
  **/
-@FeignClient(value = "user-login-service", contextId = "loginCustomFeign", fallbackFactory = LoginCustomError.class)
+@FeignClient(value = "login-service", contextId = "loginCustomFeign", fallbackFactory = LoginCustomError.class)
 public interface LoginCustomFeign extends LoginFeign {
 }
