ee72f3573bf75b41015068def19d04a178bfaeaf
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-07-28 12:27:55 +0800

    提交“增加自定义模板”

--- tiankafei-code-product/tiankafei-code-generator/tiankafei-code-generate/src/test/java/org/tiankafei/web/generate/TiankafeiCodeGenerator.java
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-generate/src/test/java/org/tiankafei/web/generate/TiankafeiCodeGenerator.java
@@ -2,14 +2,17 @@ package org.tiankafei.web.generate;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.ITypeConvert;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
 import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
@@ -18,10 +21,15 @@ import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.IColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -44,13 +52,14 @@ public class TiankafeiCodeGenerator {
     private String baseControllerClassPath = "org.tiankafei.web.common.controller.BaseController";
     private List<String> tableNameList = Arrays.asList("sys_user_test");
 
+    private AutoGenerator autoGenerator;
+
     public static void main(String[] args) throws Exception {
         new TiankafeiCodeGenerator();
     }
 
     public TiankafeiCodeGenerator() throws Exception {
-        AutoGenerator autoGenerator = new AutoGenerator();
-
+        autoGenerator = new AutoGenerator();
         // 数据源配置，通过该配置，指定需要生成代码的具体数据库
         autoGenerator.setDataSource(initDataSourceConfig());
 
@@ -77,18 +86,42 @@ public class TiankafeiCodeGenerator {
         InjectionConfig injectionConfig = new InjectionConfig(){
             @Override
             public void initMap() {
+                ConfigBuilder config = getConfig();
+
                 Map<String, Object> map = new HashMap<>();
                 map.put("shiroAuthority", shiroAuthority);
 
                 Map<String, Map<String, String>> nameMapMap = new HashMap<>();
-                List<TableInfo> tableInfoList = getConfig().getTableInfoList();
+                Map<String, List<String>> importPackageList = new HashMap<>();
+                List<TableInfo> tableInfoList = config.getTableInfoList();
                 tableInfoList.stream().forEach(tableInfo -> {
                     String name = tableInfo.getName();
+
+                    Set<String> importPackages = tableInfo.getImportPackages();
+                    List<String> voImportPackages = importPackages.stream().filter(importPackage -> {
+                        if (importPackage.endsWith("TableName")
+                                || importPackage.endsWith("IdType")
+                                || importPackage.endsWith("Model")
+                                || importPackage.endsWith("Version")
+                                || importPackage.endsWith("TableId")
+                                || importPackage.endsWith("FieldFill")
+                                || importPackage.endsWith("TableLogic")
+                                || importPackage.endsWith("TableField")) {
+                            return false;
+                        }
+                        return true;
+                    }).collect(Collectors.toList());
+                    importPackageList.put(name, voImportPackages);
 
                     Map<String, String> nameMap = new HashMap<>();
                     String entityName = tableInfo.getEntityName();
                     String entityConstName = firstToLowerCase(entityName);
                     nameMap.put("entityConstName", entityConstName);
+
+                    String voName = entityName.replace("Entity", "Vo");
+                    nameMap.put("voName", voName);
+                    nameMap.put("voConstName", firstToLowerCase(voName));
+                    nameMap.put("voPath", baseParentPath + "." + moduleName + ".vo");
 
                     String permission = underlineToColon(name);
                     nameMap.put("shiroAuthority", permission);
@@ -105,10 +138,27 @@ public class TiankafeiCodeGenerator {
                 });
 
                 map.put("name", nameMapMap);
+                map.put("packages", importPackageList);
                 setMap(map);
             }
         };
 
+        List<FileOutConfig> fileOutConfigList = new ArrayList<>();
+        fileOutConfigList.add(new FileOutConfig("/myself/vo.java.vm"){
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                String name = tableInfo.getName();
+                Map<String, Object> map = autoGenerator.getCfg().getMap();
+                Map<String, Map<String, String>> nameMap = (Map<String, Map<String, String>>) map.get("name");
+                String voName = nameMap.get(name).get("voName");
+
+                String path = outputDir + File.separator + baseParentPath.replaceAll("\\.", Matcher.quoteReplacement(File.separator)) + File.separator + moduleName + File.separator + "vo" + File.separator + voName + StringPool.DOT_JAVA;
+                System.out.println(path);
+                return path;
+            }
+        });
+
+        injectionConfig.setFileOutConfigList(fileOutConfigList);
         return injectionConfig;
     }
 
