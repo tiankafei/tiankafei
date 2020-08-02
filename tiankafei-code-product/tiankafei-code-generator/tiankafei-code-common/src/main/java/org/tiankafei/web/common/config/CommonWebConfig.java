a1243aa6b7cc6b0a54ecaf0df5bfdd99f79f0a25
tiankafei <weishuangshuang89@163.com>
2020-08-02 12:59:31 +0800

    提交“更新”

--- tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/config/CommonWebConfig.java
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/config/CommonWebConfig.java
@@ -1,9 +1,6 @@
 package org.tiankafei.web.common.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.tiankafei.web.common.filters.ApiDocFilter;
 
 /**
  * @author tiankafei
@@ -11,18 +8,6 @@ import org.tiankafei.web.common.filters.ApiDocFilter;
  **/
 @Configuration
 public class CommonWebConfig {
-
-//    @Bean
-//    public FilterRegistrationBean<ApiDocFilter> apiDocFilter() {
-//        FilterRegistrationBean<ApiDocFilter> bean = new FilterRegistrationBean<>(new ApiDocFilter());
-//        bean.setOrder(2);
-//        bean.addUrlPatterns("/doc");
-//        bean.addUrlPatterns("/docs");
-//        bean.addUrlPatterns("/doc.html");
-//        bean.addUrlPatterns("/swagger-ui.html");
-//        bean.setName("apiDocFilter");
-//        return bean;
-//    }
 
 //    @Bean
 //    public CorsFilter corsFilter() {
