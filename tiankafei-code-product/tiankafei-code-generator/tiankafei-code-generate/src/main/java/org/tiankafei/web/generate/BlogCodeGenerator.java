2bc8dd204da22b08cb55989fde9d068e8c90f22f
tiankafei <weishuangshuang89@163.com>
2020-08-02 09:22:37 +0800

    提交“更新”

---
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-generate/src/main/java/org/tiankafei/web/generate/BlogCodeGenerator.java
@@ -0,0 +1,69 @@
+package org.tiankafei.web.generate;
+
+import java.util.Arrays;
+import java.util.List;
+import org.tiankafei.web.generate.properties.CodeProperties;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ */
+public class BlogCodeGenerator extends BaseCodeGenerator {
+
+    private String author = "tiankafei";
+    private String outputDir = "E:\\gits\\tiankafei\\tiankafei-code-product\\tiankafei-service\\tiankafei-blog-service\\src\\main\\java";
+
+    private boolean shiroAuthority = false;
+    private String baseParentPath = "org.tiankafei";
+    private String moduleName = "blog";
+
+    private String baseEntityClassPath = "com.baomidou.mybatisplus.extension.activerecord.Model";
+    private String baseMapperClassPath = "com.baomidou.mybatisplus.core.mapper.BaseMapper";
+    private String baseServiceClassPath = "org.tiankafei.web.common.service.BaseService";
+    private String baseServiceImplClassPath = "org.tiankafei.web.common.service.impl.BaseServiceImpl";
+    private String baseControllerClassPath = "org.tiankafei.web.common.controller.BaseController";
+    private String baseVoClassPath = "org.tiankafei.web.common.vo.BaseQueryVo";
+    private String basePageParamClassPath = "org.tiankafei.web.common.param.OrderQueryParam";
+    private String idsParamClassPath = "org.tiankafei.web.common.param.IdsParam";
+    private String pageClassPath = "org.tiankafei.web.common.vo.Paging";
+    private String apiResultClassPath = "org.tiankafei.web.common.api.ApiResult";
+
+    private List<String> tableNameList = Arrays.asList("sys_blog_info", "sys_diary_info", "sys_global_setting");
+
+    public static void main(String[] args) throws Exception {
+        BlogCodeGenerator tiankafeiCodeGenerator = new BlogCodeGenerator();
+        tiankafeiCodeGenerator.execute();
+    }
+
+    @Override
+    protected CodeProperties initCodeProperties() {
+        CodeProperties codeProperties = new CodeProperties();
+        codeProperties.setUrl("jdbc:mysql://localhost:3306/db-blog?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true");
+        codeProperties.setDriverName("com.mysql.cj.jdbc.Driver");
+        codeProperties.setUsername("root");
+        codeProperties.setPassword("tiankafei");
+        codeProperties.setShiroAuthority(shiroAuthority);
+        codeProperties.setAuthor(author);
+        codeProperties.setOutputDir(outputDir);
+        codeProperties.setProjectPath(baseParentPath);
+        codeProperties.setModuleName(moduleName);
+        codeProperties.setSuperControllerClassPath(baseControllerClassPath);
+        codeProperties.setSuperEntityClassPath(baseEntityClassPath);
+        codeProperties.setSuperServiceClassPath(baseServiceClassPath);
+        codeProperties.setSuperServiceImplClassPath(baseServiceImplClassPath);
+        codeProperties.setSuperMapperClassPath(baseMapperClassPath);
+        codeProperties.setSuperVoClassPath(baseVoClassPath);
+        codeProperties.setSuperPageParamClassPath(basePageParamClassPath);
+        codeProperties.setTableNameList(tableNameList);
+        codeProperties.setEntity("/myself/entity.java.vm");
+        codeProperties.setService("/myself/service.java.vm");
+        codeProperties.setServiceImpl("/myself/serviceImpl.java.vm");
+        codeProperties.setMapper("/myself/mapper.java.vm");
+        codeProperties.setXml("/myself/mapper.xml.vm");
+        codeProperties.setController("/myself/controller.java.vm");
+        codeProperties.setIdsParamClassPath(idsParamClassPath);
+        codeProperties.setPageClassPath(pageClassPath);
+        codeProperties.setApiResultClassPath(apiResultClassPath);
+        return codeProperties;
+    }
+}
