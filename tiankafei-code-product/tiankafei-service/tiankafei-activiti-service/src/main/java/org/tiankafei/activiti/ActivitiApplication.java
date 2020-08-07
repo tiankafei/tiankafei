11c248b71b4b943ef87ef5eb4fba00ff7ffb27e2
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-08-07 12:21:50 +0800

    提交“更新依赖配置”

--- tiankafei-code-product/tiankafei-service/tiankafei-activiti-service/src/main/java/org/tiankafei/activiti/ActivitiApplication.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-activiti-service/src/main/java/org/tiankafei/activiti/ActivitiApplication.java
@@ -1,9 +1,12 @@
 package org.tiankafei.activiti;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,7 +17,9 @@ import org.springframework.context.annotation.ComponentScan;
  * @author tiankafei
  * @since 1.0
  **/
-@SpringCloudApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 @EntityScan(basePackages = "org.tiankafei")
 @MapperScan(basePackages = {"org.tiankafei.**.mapper"})
 @ComponentScan(basePackages = {"org.tiankafei"})
