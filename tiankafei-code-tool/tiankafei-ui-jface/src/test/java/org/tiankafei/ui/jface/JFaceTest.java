39a3a9d15af2a24260ac4af81cac2259632ce4de
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-09-03 20:02:34 +0800

    提交“增加jface依赖”

---
+++ tiankafei-code-tool/tiankafei-ui-jface/src/test/java/org/tiankafei/ui/jface/JFaceTest.java
@@ -0,0 +1,17 @@
+package org.tiankafei.ui.jface;
+
+import org.junit.Test;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ */
+public class JFaceTest {
+
+    @Test
+    public void test(){
+
+
+    }
+
+}
