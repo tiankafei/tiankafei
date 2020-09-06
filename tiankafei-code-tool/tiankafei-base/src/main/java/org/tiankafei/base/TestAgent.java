0b2b8c61cb94e4ad7593e0f2e47bc26be34e71ff
tiankafei <weishuangshuang89@163.com>
2020-09-06 18:03:13 +0800

    提交“自定义agent测试类”

--- tiankafei-code-tool/tiankafei-base/src/main/java/org/tiankafei/base/TestAgent.java
+++ tiankafei-code-tool/tiankafei-base/src/main/java/org/tiankafei/base/TestAgent.java
@@ -10,6 +10,23 @@ public class TestAgent {
 
     public static void main(String[] args) {
         System.out.println(TiankafeiAgent.sizeOf(new Object()));
+        System.out.println(TiankafeiAgent.sizeOf(new int[] {}));
+        System.out.println(TiankafeiAgent.sizeOf(new P()));
+    }
+
+    private static class P {
+        //8 _markword
+        //4 _oop指针
+        int id;         //4
+        String name;    //4
+        int age;        //4
+
+        byte b1;        //1
+        byte b2;        //1
+
+        Object o;       //4
+        byte b3;        //1
+
     }
 
 }
