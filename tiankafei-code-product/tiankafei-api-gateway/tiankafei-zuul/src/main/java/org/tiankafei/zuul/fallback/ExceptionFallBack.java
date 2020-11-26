c46e2fc8fbd23a9305c69b502d53b69e7b4f0e3a
weishuangshuang <weishuangshuang@thtf.com.cn>
2020-11-26 13:38:29 +0800

    提交“网关异常回调，返回统一格式”

---
+++ tiankafei-code-product/tiankafei-api-gateway/tiankafei-zuul/src/main/java/org/tiankafei/zuul/fallback/ExceptionFallBack.java
@@ -0,0 +1,69 @@
+package org.tiankafei.zuul.fallback;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.tiankafei.web.common.api.ApiResult;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 网关异常回调，返回统一格式
+ * @author 甜咖啡
+ */
+@Component
+public class ExceptionFallBack implements FallbackProvider {
+
+    @Override
+    public String getRoute() {
+        // 对所有服务生效
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse(){
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                // 返回通用的结构
+                ApiResult apiResult = ApiResult.fail();
+                return new ByteArrayInputStream(JSONObject.toJSONBytes(apiResult));
+            }
+
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.BAD_REQUEST;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.BAD_REQUEST.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "message is exception";
+            }
+
+            @Override
+            public void close() {
+
+            }
+        };
+    }
+
+}
