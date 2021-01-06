294c69ac7dc8e8c64367484adf5ad24097f03072
weishuangshuang <weishuangshuang@thtf.com.cn>
2021-01-06 11:50:00 +0800

    提交“提交elasticsearch服务工程”

---
+++ tiankafei-code-product/tiankafei-service/tiankafei-elasticsearch-service/tiankafei-elasticsearch-server/src/main/java/org/tiankafei/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,25 @@
+package org.tiankafei.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+@SpringCloudApplication
+@EntityScan(basePackages = "org.tiankafei")
+@ComponentScan(basePackages = {"org.tiankafei"})
+@EnableFeignClients(basePackages = {"org.tiankafei"})
+@EnableHystrixDashboard
+public class ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+
+}
---
+++ tiankafei-code-product/tiankafei-service/tiankafei-elasticsearch-service/tiankafei-elasticsearch-server/src/test/java/org/tiankafei/elasticsearch/GeneralElasticsearchTest.java
@@ -0,0 +1,8 @@
+package org.tiankafei.elasticsearch;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+public class GeneralElasticsearchTest {
+}
