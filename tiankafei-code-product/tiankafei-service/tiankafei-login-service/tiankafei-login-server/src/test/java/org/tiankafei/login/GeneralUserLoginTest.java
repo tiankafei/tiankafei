4b070585e6692c4af4d26a48cc62cee44d623750
tiankafei <weishuangshuang89@163.com>
2021-02-03 23:34:31 +0800

    提交“更新版本，删除不必要的工程”

--- tiankafei-code-product/tiankafei-service/tiankafei-login-service/tiankafei-login-server/src/test/java/org/tiankafei/login/GeneralUserLoginTest.java
+++
@@ -1,8 +0,0 @@
-package org.tiankafei.login;
-
-/**
- * @author tiankafei
- * @since 1.0
- **/
-public class GeneralUserLoginTest {
-}
--- tiankafei-code-product/tiankafei-service/tiankafei-user-common/src/test/java/org/tiankafei/user/GeneralUserCommonTest.java
+++
@@ -1,8 +0,0 @@
-package org.tiankafei.user;
-
-/**
- * @author tiankafei
- * @since 1.0
- **/
-public class GeneralUserCommonTest {
-}
