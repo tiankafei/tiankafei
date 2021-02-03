c3e72b3e00a956e3d880e47a1632047aa23dbfbd
tiankafei <weishuangshuang89@163.com>
2021-02-04 00:28:40 +0800

    提交“去掉多余的配置”

--- tiankafei-code-product/tiankafei-service/tiankafei-multi-datasource-jpa-service/src/main/java/org/tiankafei/multidatasource/jpa/MultiDatasourceJpaApplication.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-multi-datasource-jpa-service/src/main/java/org/tiankafei/multidatasource/jpa/MultiDatasourceJpaApplication.java
@@ -3,18 +3,15 @@ package org.tiankafei.multidatasource.jpa;
 import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author tiankafei
  * @since 1.0
  **/
 @SpringCloudApplication
-@EntityScan(basePackages = "org.tiankafei")
 @MapperScan(basePackages = {"org.tiankafei.**.mapper"})
 @EnableFeignClients(basePackages = {"org.tiankafei", "com.ruoyi"})
 @EnableHystrixDashboard
