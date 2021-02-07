c3727b6618ac5cf7eb4e0d337cd3ca6219358427
tiankafei <weishuangshuang89@163.com>
2021-02-07 09:40:40 +0800

    提交“更新表结构”

--- tiankafei-code-product/tiankafei-service/tiankafei-db/src/main/resources/sql/db-features.sql
+++ tiankafei-code-product/tiankafei-service/tiankafei-db/src/main/resources/sql/db-features.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : root
+ Source Server         : localhost
  Source Server Type    : MySQL
  Source Server Version : 80020
  Source Host           : localhost:3306
- Source Schema         : db-features
+ Source Schema         : db-blog
 
  Target Server Type    : MySQL
  Target Server Version : 80020
  File Encoding         : 65001
 
- Date: 21/08/2020 22:25:48
+ Date: 07/02/2021 09:35:33
 */
 
 SET NAMES utf8mb4;
@@ -24,26 +24,33 @@ DROP TABLE IF EXISTS `sys_attributes_info`;
 CREATE TABLE `sys_attributes_info`  (
   `id` bigint(0) NOT NULL COMMENT '主键id',
   `features_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '功能代码',
-  `attributes_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '属性代码',
-  `attributes_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '属性名称',
+  `code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '属性代码',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '属性名称',
   `data_type` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '数据类型',
   `data_length` int(0) NULL DEFAULT NULL COMMENT '数据长度',
   `data_precision` tinyint(0) NULL DEFAULT NULL COMMENT '数据精度',
   `is_null` tinyint(1) NULL DEFAULT NULL COMMENT '是否允许为空',
   `default_value` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '默认值',
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '功能描述',
-  `version` int(0) NULL DEFAULT NULL COMMENT '乐观锁版本',
-  `delete_mark` int(0) NULL DEFAULT 0 COMMENT '逻辑删除字段',
-  `create_time` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` timestamp(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `create_user_id` bigint(0) NULL DEFAULT NULL COMMENT '创建用户ID',
-  `update_user_id` bigint(0) NULL DEFAULT NULL COMMENT '修改用户ID',
+  `is_insert_property` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '是否新增属性（1是）',
+  `is_edit_property` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '是否编辑属性（1是）',
+  `is_list_property` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '是否列表属性（1是）',
+  `is_query_property` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '是否查询属性（1是）',
+  `is_default_query_property` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '是否默认查询属性（1是）',
+  `query_type` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT 'EQ' COMMENT '查询方式（等于、不等于、大于、小于、范围）',
+  `fill_type` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '填写类型（文本框、文本域、下拉框、复选框、单选框、日期控件）',
+  `search_type` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '查询类型（文本框、文本域、下拉框、复选框、单选框、日期控件）',
+  `data_source` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '数据来源：1:json，2:url，3字典',
+  `data_structure` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '数据来源为json的数据结构',
+  `data_url` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '数据来原为url',
+  `dict_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '' COMMENT '数据原来为字典的字典类型',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '0' COMMENT '删除标志（0代表存在 2代表删除）',
+  `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '' COMMENT '创建者',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '' COMMENT '更新者',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '功能的属性注册表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of sys_attributes_info
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for sys_features_info
@@ -51,22 +58,19 @@ CREATE TABLE `sys_attributes_info`  (
 DROP TABLE IF EXISTS `sys_features_info`;
 CREATE TABLE `sys_features_info`  (
   `id` int(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `features_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '功能代码',
-  `features_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '功能名称',
+  `code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '功能代码',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '功能名称',
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '0' COMMENT '帐号状态（0正常 1停用）',
   `features_table_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '功能的数据表',
   `serial_number` int(0) NULL DEFAULT NULL COMMENT '序号',
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '功能描述',
-  `version` int(0) NULL DEFAULT NULL COMMENT '乐观锁版本',
-  `delete_mark` int(0) NULL DEFAULT 0 COMMENT '逻辑删除字段',
-  `create_time` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` timestamp(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `create_user_id` bigint(0) NULL DEFAULT NULL COMMENT '创建用户ID',
-  `update_user_id` bigint(0) NULL DEFAULT NULL COMMENT '修改用户ID',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '0' COMMENT '删除标志（0代表存在 2代表删除）',
+  `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '' COMMENT '创建者',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT '' COMMENT '更新者',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '功能注册表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of sys_features_info
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '功能注册表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
