8692a9d48af729b2d4732a6ca7bd15998a6d0a5d
tiankafei <weishuangshuang89@163.com>
2021-02-07 13:14:11 +0800

    提交“删除功能配置相关代码”

---
+++ tiankafei-code-product/tiankafei-service/tiankafei-db/src/main/java/org/tiankafei/db/config/DbConfig.java
@@ -0,0 +1,16 @@
+package org.tiankafei.db.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+@Component
+@ComponentScan(basePackages = "org.tiankafei.db")
+public class DbConfig {
+
+
+
+}
