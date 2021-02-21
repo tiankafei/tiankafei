67716d8998932377384ce2c276d7fa012e7a43df
tiankafei <weishuangshuang89@163.com>
2021-02-21 11:02:33 +0800

    提交“更新依赖及配置”

--- tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/config/WebCommonConfig.java
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/config/WebCommonConfig.java
@@ -1,15 +1,10 @@
 package org.tiankafei.web.common.config;
 
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import java.math.BigInteger;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 
 /**
  * @author tiankafei
@@ -20,24 +15,9 @@ import org.springframework.web.client.RestTemplate;
 public class WebCommonConfig {
 
     @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setErrorHandler(new DefaultResponseErrorHandler());
-        return restTemplate;
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jacksonLongToSring() {
-        Jackson2ObjectMapperBuilderCustomizer customizer = new Jackson2ObjectMapperBuilderCustomizer() {
-            @Override
-            public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
-                jacksonObjectMapperBuilder.serializerByType(BigInteger.class, ToStringSerializer.instance);
-                jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
-                jacksonObjectMapperBuilder.serializerByType(Long.TYPE, ToStringSerializer.instance);
-            }
-        };
-        return customizer;
+    public PathMatcher pathMatcher(){
+        PathMatcher pathMatcher = new AntPathMatcher();
+        return pathMatcher;
     }
 
 }
--- tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/utils/CommonUtil.java
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/utils/CommonUtil.java
@@ -3,13 +3,19 @@ package org.tiankafei.web.common.utils;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.List;
-import java.util.regex.Pattern;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 
 /**
  * @author tiankafei
  * @since 1.0
  **/
 public class CommonUtil {
+
+    /**
+     * ant风格路径匹配对象
+     */
+    private static PathMatcher pathMatcher = new AntPathMatcher();
 
     private CommonUtil() {
     }
@@ -25,74 +31,12 @@ public class CommonUtil {
         if (CollectionUtils.isNotEmpty(urls)) {
             for (int index = 0, length = urls.size(); index < length; index++) {
                 String url = urls.get(index);
-                String regPath = getRegPath(url);
-                if (filterUrls(regPath, path)) {
+                if (pathMatcher.match(url, path)) {
                     return Boolean.TRUE;
                 }
             }
         }
         return Boolean.FALSE;
-    }
-
-    /**
-     * 通配符模式
-     *
-     * @param excludePath - 不过滤地址
-     * @param reqUrl      - 请求地址
-     * @return
-     */
-    private static boolean filterUrls(String excludePath, String reqUrl) {
-        String regPath = getRegPath(excludePath);
-        return Pattern.compile(regPath).matcher(reqUrl).matches();
-    }
-
-    /**
-     * 将通配符表达式转化为正则表达式
-     *
-     * @param path
-     * @return
-     */
-    private static String getRegPath(String path) {
-        char[] chars = path.toCharArray();
-        int len = chars.length;
-        StringBuilder sb = new StringBuilder();
-        boolean preX = false;
-        for (int i = 0; i < len; i++) {
-            // 遇到*字符
-            if (chars[i] == '*') {
-                // 如果是第二次遇到*，则将**替换成.*
-                if (preX) {
-                    sb.append(".*");
-                    preX = false;
-                }
-                // 如果是遇到单星，且单星是最后一个字符，则直接将*转成[^/]*
-                else if (i + 1 == len) {
-                    sb.append("[^/]*");
-                }
-                // 否则单星后面还有字符，则不做任何动作，下一把再做动作
-                else {
-                    preX = true;
-                    continue;
-                }
-            }
-            // 遇到非*字符
-            else {
-                // 如果上一把是*，则先把上一把的*对应的[^/]*添进来
-                if (preX) {
-                    sb.append("[^/]*");
-                    preX = false;
-                }
-                // 接着判断当前字符是不是?，是的话替换成.
-                if (chars[i] == '?') {
-                    sb.append('.');
-                }
-                // 不是?的话，则就是普通字符，直接添进来
-                else {
-                    sb.append(chars[i]);
-                }
-            }
-        }
-        return sb.toString();
     }
 
 }
