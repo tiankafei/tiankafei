b7a466b6856e61d88a589be103f3c15e260ce559
tiankafei <weishuangshuang89@163.com>
2021-03-27 22:17:32 +0800

    提交“更新”

--- tiankafei-code-tool/tiankafei-rocksdb/src/test/java/org/tiankafei/rocksdb/RocksdbTest.java
+++ tiankafei-code-tool/tiankafei-rocksdb/src/test/java/org/tiankafei/rocksdb/RocksdbTest.java
@@ -5,8 +5,7 @@ import java.util.List;
 import java.util.Properties;
 import org.junit.Before;
 import org.junit.Test;
-import org.tiankafei.base.exceptions.BaseException;
-import org.tiankafei.base.util.PropertiesUtil;
+import org.tiankafei.common.util.PropertiesUtil;
 import org.tiankafei.rocksdb.util.RocksdbUtil;
 
 /**
@@ -21,37 +20,28 @@ public class RocksdbTest {
 
     @Before
     public void init() {
-        try {
-            Properties rocksdbProperties = PropertiesUtil.getInstance("rocksdb.properties");
-            rocksdbPartitionFilePath = PropertiesUtil.getStringValue(rocksdbProperties, "rocksdbPartitionFilePath");
-            rocksdbPath = PropertiesUtil.getStringValue(rocksdbProperties, "rocksdbFilePath");
-        } catch (BaseException e) {
-            e.printStackTrace();
-        }
+        Properties rocksdbProperties = PropertiesUtil.getInstance("rocksdb.properties");
+        rocksdbPartitionFilePath = PropertiesUtil.getStringValue(rocksdbProperties, "rocksdbPartitionFilePath");
+        rocksdbPath = PropertiesUtil.getStringValue(rocksdbProperties, "rocksdbFilePath");
     }
 
 
     @Test(expected = Exception.class)
     public void rocksdbTest() {
-        try {
-            List<String> rocksdbPartitionNameList = Lists.newArrayList();
-            rocksdbPartitionNameList.add("testPartition");
-            boolean rocksdbPartitionFlag = true;
-            RocksdbUtil rocksdbUtil = new RocksdbUtil(rocksdbPartitionFlag, rocksdbPartitionFilePath, rocksdbPartitionNameList);
-            testRocksdb(rocksdbUtil);
-            System.out.println("rocksdb分区测试=======================");
+        List<String> rocksdbPartitionNameList = Lists.newArrayList();
+        rocksdbPartitionNameList.add("testPartition");
+        boolean rocksdbPartitionFlag = true;
+        RocksdbUtil rocksdbUtil = new RocksdbUtil(rocksdbPartitionFlag, rocksdbPartitionFilePath, rocksdbPartitionNameList);
+        testRocksdb(rocksdbUtil);
+        System.out.println("rocksdb分区测试=======================");
 
-            rocksdbPartitionFlag = false;
-            rocksdbUtil = new RocksdbUtil(rocksdbPartitionFlag, rocksdbPath);
-            testRocksdb(rocksdbUtil);
-            System.out.println("rocksdb测试=======================");
-        } catch (BaseException e) {
-            e.printStackTrace();
-        }
-
+        rocksdbPartitionFlag = false;
+        rocksdbUtil = new RocksdbUtil(rocksdbPartitionFlag, rocksdbPath);
+        testRocksdb(rocksdbUtil);
+        System.out.println("rocksdb测试=======================");
     }
 
-    private void testRocksdb(RocksdbUtil rocksdbUtil) throws BaseException {
+    private void testRocksdb(RocksdbUtil rocksdbUtil) {
         String key = rocksdbUtil.getKey("Hello");
         String value = "Word";
         rocksdbUtil.setValue(key, value);
@@ -72,7 +62,6 @@ public class RocksdbTest {
 
         rocksdbUtil.removeKey(key1);
         System.out.println(rocksdbUtil.getValueArray(keyList));
-
     }
 
 }
