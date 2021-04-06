28799b99e417dbab882256adc558b14f27b64351
tiankafei <weishuangshuang89@163.com>
2021-04-06 21:30:19 +0800

    提交“更新打包报错问题”

--- tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/service/impl/QueryDbNameService.java
+++ tiankafei-code-product/tiankafei-code-generator/tiankafei-code-common/src/main/java/org/tiankafei/web/common/service/impl/QueryDbNameService.java
@@ -1,5 +1,6 @@
 package org.tiankafei.web.common.service.impl;
 
+import com.ruoyi.common.core.exception.DaoException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,13 @@ public class QueryDbNameService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public String getDbName() throws Exception {
+    public String getDbName() throws DaoException {
         try {
             Connection connection = jdbcTemplate.getDataSource().getConnection();
             return connection.getCatalog();
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new Exception("获取数据库名发生异常");
+            throw new DaoException("获取数据库名发生异常");
         }
     }
 
