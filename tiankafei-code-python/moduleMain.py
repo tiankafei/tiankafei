cba57c743078264fd24837be462a5d6b75f54d2b
tiankafei <weishuangshuang89@163.com>
2021-04-06 23:20:48 +0800

    提交“python示例”

--- tiankafei-code-python/moduleMain.py
+++ tiankafei-code-python/moduleMain.py
@@ -5,3 +5,8 @@ import package1.ModuleA as A
 # import package1.ModuleB as A
 
 print(A.a)
+
+# windows打包可执行文件
+# 安装以来
+# pip install PyInstaller
+# pyinstaller -F E:\gits\tiankafei\tiankafei-code-python\moduleMain.py
