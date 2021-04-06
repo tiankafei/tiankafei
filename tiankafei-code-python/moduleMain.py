9f8a3c9d41c57dbc6099bb0bde67152622807e57
tiankafei <weishuangshuang89@163.com>
2021-04-06 20:47:32 +0800

    提交“python示例”

---
+++ tiankafei-code-python/moduleMain.py
@@ -0,0 +1,7 @@
+# 作者：甜咖啡
+# 新建时间：2021/4/6 20:29
+
+import package1.ModuleA as A
+# import package1.ModuleB as A
+
+print(A.a)
---
+++ tiankafei-code-python/sysMain.py
@@ -0,0 +1,31 @@
+# 作者：甜咖啡
+# 新建时间：2021/4/6 20:33
+
+import sys
+import time
+import os
+import calendar
+import urllib.request
+import json
+import re
+import math
+import decimal
+import logging
+import schedule
+
+print(sys.getsizeof(24))
+print(sys.getsizeof(45))
+print(sys.getsizeof(True))
+print(sys.getsizeof(False))
+print('===============================================')
+
+print(time.time())
+print(time.localtime(time.time()))
+print('===============================================')
+
+# print(urllib.request.urlopen('http://www.baidu.com').read())
+
+print(math.pi)
+print(math.ceil(1.23))
+print('===============================================')
+
