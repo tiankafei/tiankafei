3329902cb40d86790a22de15927cd31ad107b88d
weishuangshuang <weishuangshuang@thtf.com.cn>
2021-04-07 12:35:21 +0800

    提交“算法参数梳理”

--- tiankafei-code-python/analysis/NormalCheck.py
+++ tiankafei-code-python/analysis/NormalCheck.py
@@ -12,8 +12,13 @@ names = list(datas.columns)
 a1 = datas['分析项1_定类'].to_list()
 a2 = datas['分析项2_定量'].to_list()
 
+# K-S检验 TODO
+res1 = st.kstest(a2, a1, args=a1)
+print(res1.statistic)
+print(res1.pvalue)
 
-st.kstest()
 
-
-st.shapiro()
+# S-W检验
+res2 = st.shapiro(a2)
+print(res2.statistic)
+print(res2.pvalue)
