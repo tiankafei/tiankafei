b8ba71a2da6a5e53e442a3d18fe918d03658799c
weishuangshuang <weishuangshuang@thtf.com.cn>
2021-04-07 10:14:17 +0800

    提交“算法参数梳理”

--- tiankafei-code-python/analysis/LinearRegression.py
+++ tiankafei-code-python/analysis/LinearRegression.py
@@ -2,3 +2,18 @@
 
 # scipy.stats.linregress(一元线性回归)
 # sklearn.linear_model.LinearRegression（多元线性回归）
+# statsmodels.（新的多元线性回归）
+
+# TODO 有待测试拿结果
+
+import pandas as pd
+import statsmodels.formula.api as sm
+
+datas = pd.read_excel(r'D:\python\linear_regression.xlsx') # 读取 excel 数据，引号里面是 excel 文件的位置
+
+model = sm.ols('测试数据1~测试数据2', datas).fit() # 构建最小二乘模型并拟合，
+                               #此时不用单独输入 x，y了，而是将自变量与因变量用统计语言公式表示，将全部数据导入
+
+s = model.summary()
+
+print(s) # 输出回归结果
--- tiankafei-code-python/analysis/Variance.py
+++ tiankafei-code-python/analysis/Variance.py
@@ -1,3 +1,14 @@
 # 方差分析算法
 
 # scipy.stats.f_oneway
+
+import scipy.stats as st
+
+a = [1, 2, 3, 4, 5, 6, 7]
+b = [1, 2, 3, 4, 5, 6, 7]
+c = [1, 2, 3, 4, 5, 6, 7]
+
+res = st.f_oneway(a, b, c)
+
+print(res.statistic)
+print(res.pvalue)
