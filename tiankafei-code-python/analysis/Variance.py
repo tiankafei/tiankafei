1255b6ea3a83f1be6fc584c763a207c750588eb0
weishuangshuang <weishuangshuang@thtf.com.cn>
2021-04-07 10:38:10 +0800

    提交“算法参数梳理”

--- tiankafei-code-python/analysis/Variance.py
+++ tiankafei-code-python/analysis/Variance.py
@@ -2,13 +2,22 @@
 
 # scipy.stats.f_oneway
 
+import pandas as pd
 import scipy.stats as st
 
-a = [1, 2, 3, 4, 5, 6, 7]
-b = [1, 2, 3, 4, 5, 6, 7]
-c = [1, 2, 3, 4, 5, 6, 7]
+# 1.先联调方法
+# 2.包装，接收外部参数，返回结果
 
-res = st.f_oneway(a, b, c)
+datas = pd.read_excel(r'D:\python\data.xls') # 读取 excel 数据，引号里面是 excel 文件的位置
+names = list(datas.columns)
+
+lst = []
+for clumnName in names:
+    a = datas[clumnName].to_list()
+    lst.append(a)
+print(lst)
+
+res = st.f_oneway(*lst)
 
 print(res.statistic)
 print(res.pvalue)
