de845910b0e2f65706e41a44f937b90223799889
weishuangshuang <weishuangshuang@thtf.com.cn>
2021-04-08 10:52:03 +0800

    提交“分类汇总方法封装”

--- tiankafei-code-python/analysis/ClassifyCollection.py
+++ tiankafei-code-python/analysis/ClassifyCollection.py
@@ -3,53 +3,97 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as st
-
-datas = pd.read_excel(r'D:\python\data.xls')  # 读取 excel 数据，引号里面是 excel 文件的位置
-names = list(datas.columns)
-
-a1 = datas['分析项1_定类'].to_list()
-a2 = datas['分析项2_定量'].to_list()
-
-lst = []
-v1 = []
-v2 = []
-for s1, s2 in zip(a1, a2):
-    if s1 == 1:
-        v1.append(s2)
-    elif s1 == 2:
-        v2.append(s2)
-lst.append(v1)
-lst.append(v2)
-
-def mean_confidence_interval(data, confidence=0.95):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), st.sem(a)
-    h = se * st.t.ppf((1 + confidence) / 2., n-1)
-    return m-h, m+h
+import json
 
 
-for data in lst:
-    n = len(data)
-    ave = np.average(data)
-    sum = np.sum(data)
-    cen = np.median(data)
-    max = np.max(data)
-    min = np.min(data)
-    std = np.std(data, ddof=1)
+def execute_analysis_all(string):
+    json_list = json.loads(string)
+    result = []
+    for data in json_list:
+        name = data['name']
+        value_list = data['valueList']
+
+        res = execute_analysis(name, value_list)
+        result.append(res)
+    return result
+
+
+def execute_analysis(name, data):
+    count_v = len(data)
+    avg_v = np.average(data)
+    sum_v = np.sum(data)
+    cen_v = np.median(data)
+    max_v = np.max(data)
+    min_v = np.min(data)
+    std_v = np.std(data, ddof=1)
     v25 = st.scoreatpercentile(data, 25, interpolation_method='lower')
     v75 = st.scoreatpercentile(data, 75, interpolation_method='lower')
     v90 = st.scoreatpercentile(data, 90, interpolation_method='lower')
     v95 = st.scoreatpercentile(data, 95, interpolation_method='lower')
     v99 = st.scoreatpercentile(data, 99, interpolation_method='lower')
-    sem = st.sem(data, ddof=1, nan_policy='omit')
+    sem_v = st.sem(data, ddof=1, nan_policy='omit')
     avg95 = mean_confidence_interval(data, confidence=0.95)
-    variation = np.var(data, ddof=1)
-    fd = st.kurtosis(data, bias=False, nan_policy='omit')
-    pd = st.skew(data, bias=False, nan_policy='omit')
+    fc_v = np.var(data, ddof=1)
+    fd_v = st.kurtosis(data, bias=False, nan_policy='omit')
+    pd_v = st.skew(data, bias=False, nan_policy='omit')
 
-    print('总数:', n, ';平均值', ave, ';求和', sum, ';中位数', cen, ';最大值', max, ';最小值', min, ';标准差', std, ';25分位数',
-          v25, ';75分位数', v75, ';90分位数', v90, ';95分位数', v95, ';99分位数', v99, ';标准误', sem, ';均值95%CI(LL)',
-          avg95[0], ';均值95%CI(UL)', avg95[1], ';极差', max - min, ';方差', variation, ';峰度', fd, ';偏度', pd)
+    res = dict()
+    res['count'] = count_v
+    res['avg'] = avg_v
+    res['sum'] = sum_v
+    res['cen'] = cen_v
+    res['max'] = max_v
+    res['min'] = min_v
+    res['std'] = std_v
+    res['v25'] = v25
+    res['v75'] = v75
+    res['v90'] = v90
+    res['v95'] = v95
+    res['v99'] = v99
+    res['sem'] = sem_v
+    res['avg95ll'] = avg95[0]
+    res['avg95ul'] = avg95[1]
+    res['jc'] = max_v - min_v
+    res['fc'] = fc_v
+    res['fd'] = fd_v
+    res['pd'] = pd_v
+    res['name'] = name
+    return res
+
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), st.sem(a)
+    h = se * st.t.ppf((1 + confidence) / 2., n - 1)
+    return m - h, m + h
+
+
+if __name__ == '__main__':
+    datas = pd.read_excel(r'D:\python\data.xls')  # 读取 excel 数据，引号里面是 excel 文件的位置
+    names = list(datas.columns)
+
+    a1 = datas['分析项1_定类'].to_list()
+    a2 = datas['分析项2_定量'].to_list()
+
+    lst = []
+    v1 = []
+    v2 = []
+    for s1, s2 in zip(a1, a2):
+        if s1 == 1:
+            v1.append(s2)
+        elif s1 == 2:
+            v2.append(s2)
+    lst.append(v1)
+    lst.append(v2)
+
+    res1 = execute_analysis('', v1)
+    print(res1)
+    res2 = execute_analysis('', v2)
+    print(res2)
+
+    res3 = execute_analysis_all('[{"name":"测试1","valueList":[1,2,3,4,5,6,7,8,9]}]')
+    print(res3)
+
 
 
