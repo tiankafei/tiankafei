dedad292dec33ffbc23b352597648083e601faf1
tiankafei <weishuangshuang89@163.com>
2021-04-14 22:41:29 +0800

    提交"go语言demo"

--- tiankafei-code-go/src/main/go/demo4.go
+++ tiankafei-code-go/src/main/go/demo4.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var age1 = 18
+	fmt.Println(age1)
+	fmt.Println(&age1)
+
+	var age2 *int = &age1
+	fmt.Println(age2)
+	fmt.Println(*age2)
+	fmt.Println(&age2)
+
+	*age2 = 20
+	fmt.Println(age1)
+
+	var a = 1
+	var b = 0
+	fmt.Println(a, b)
+	a, b = repalce(a, b)
+	fmt.Println(a, b)
+}
+
+func repalce(a int, b int) (a1 int, b1 int) {
+	return b, a
+}
