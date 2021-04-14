ca2c003818bbe6703665606f8ddc42cb19b82b08
tiankafei <weishuangshuang89@163.com>
2021-04-14 21:37:56 +0800

    提交"go语言demo"

--- tiankafei-code-go/src/main/go/demo3.go
+++ tiankafei-code-go/src/main/go/demo3.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var c1 byte = 'a'
@@ -26,5 +29,41 @@ func main() {
 	//只能是单引号的字符
 	//var c5 int = "中"
 	//fmt.Println(c5)
+	fmt.Println("--------------------------------------------------")
+
+	fmt.Println(fmt.Sprintf("%d", 20))
+	sprintf := fmt.Sprintf("%v", 20.123456789)
+	fmt.Printf("%T", sprintf)
+	fmt.Println()
+	fmt.Println(sprintf)
+
+	fmt.Println("--------------------------------------------------")
+
+	var age1 = 12
+	age2 := 123
+	fmt.Printf("%T", age1)
+	fmt.Println()
+	fmt.Printf("%T", age2)
+	fmt.Println()
+	fmt.Printf("%T", 123)
+	fmt.Println()
+	fmt.Println(strconv.FormatInt(123, 10))
+	fmt.Println(strconv.FormatInt(int64(age1), 10))
+	fmt.Println(strconv.FormatInt(int64(age2), 10))
+	fmt.Println(strconv.FormatFloat(123.123, 'f', 6, 64))
+
+	fmt.Println("--------------------------------------------------")
+
+	a, _ := strconv.ParseInt("123", 10, 16)
+	fmt.Printf("%T:::", a)
+	fmt.Println(a)
+
+	b, _ := strconv.ParseFloat("123.123", 64)
+	fmt.Printf("%T:::", b)
+	fmt.Println(b)
+
+	c, _ := strconv.ParseFloat("abcdefg", 64)
+	fmt.Printf("%T:::", c)
+	fmt.Println(c)
 
 }
