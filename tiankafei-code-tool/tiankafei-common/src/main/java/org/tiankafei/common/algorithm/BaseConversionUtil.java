ccdd79c059869a4e8a32d3a0c3222c00a92e28ae
tiankafei <weishuangshuang89@163.com>
2021-04-18 15:36:59 +0800

    提交“更新”

--- tiankafei-code-tool/tiankafei-common/src/main/java/org/tiankafei/common/algorithm/BaseConversionUtil.java
+++ tiankafei-code-tool/tiankafei-common/src/main/java/org/tiankafei/common/algorithm/BaseConversionUtil.java
@@ -15,12 +15,36 @@ public class BaseConversionUtil {
         System.out.println("\t\t4的二进制 ：" + printTwoBase(4));
         System.out.println(num + "的二进制 ：" + printTwoBase(Integer.MAX_VALUE));
         System.out.println("\t\t-1的二进制 ：" + printTwoBase(-1));
+        System.out.println("========================");
+
+        int times = 10000000;
+        int maxValue = 100;
+        for (int index = 0; index < times; index++) {
+            int value = randomValue(100000);
+//            boolean flag = (value % 2) == mod2(value);
+//            boolean flag = (value % 4) == mod4(value);
+//            boolean flag = (value % 8) == mod8(value);
+//            boolean flag = (value % 16) == mod16(value);
+//            boolean flag = (value % 32) == mod32(value);
+//            boolean flag = (value % 64) == mod64(value);
+//            boolean flag = (value % 128) == mod128(value);
+//            boolean flag = (value % 256) == mod256(value);
+//            boolean flag = (value % 512) == mod512(value);
+//            boolean flag = (value % 1024) == mod1024(value);
+//            boolean flag = (value % 2048) == mod2048(value);
+            boolean flag = (value % 4096) == mod4096(value);
+            if (!flag) {
+                System.out.println("例外情况：值是：" + value);
+            }
+        }
+        System.out.println("========================");
     }
 
     /**
      * 打印数字的二进制
-     * @param num   数字
-     * @return      二进制
+     *
+     * @param num 数字
+     * @return 二进制
      */
     public static String printTwoBase(int num) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -29,6 +53,64 @@ public class BaseConversionUtil {
             stringBuilder.append((num & (1 << i)) == 0 ? "0" : "1");
         }
         return stringBuilder.toString();
+    }
+
+    /**
+     * 随机生成一个[0, MaxValue)的值
+     *
+     * @param maxValue 最大值
+     * @return
+     */
+    public static Integer randomValue(int maxValue) {
+        return (int) (Math.random() * maxValue);
+    }
+
+    public static Integer mod2(int num) {
+        return num & 1;
+    }
+
+    public static Integer mod4(int num) {
+        return num & 3;
+    }
+
+    public static Integer mod8(int num) {
+        return num & 7;
+    }
+
+    public static Integer mod16(int num) {
+        return num & 15;
+    }
+
+    public static Integer mod32(int num) {
+        return num & 31;
+    }
+
+    public static Integer mod64(int num) {
+        return num & 63;
+    }
+
+    public static Integer mod128(int num) {
+        return num & 127;
+    }
+
+    public static Integer mod256(int num) {
+        return num & 255;
+    }
+
+    public static Integer mod512(int num) {
+        return num & 511;
+    }
+
+    public static Integer mod1024(int num) {
+        return num & 1023;
+    }
+
+    public static Integer mod2048(int num) {
+        return num & 2047;
+    }
+
+    public static Integer mod4096(int num) {
+        return num & 4095;
     }
 
 }
