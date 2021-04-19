d644ebe27975945cec228518ceaaf9f2325f1ca8
weishuangshuang <weishuangshuang@thtf.com.cn>
2021-04-19 19:53:55 +0800

    提交“提交go语言的demo”

--- tiankafei-code-go/src/main/go/demo4.go
+++ tiankafei-code-go/src/main/go/demo4.go
@@ -32,6 +32,30 @@ func main() {
 	var age4 int64 = 123
 	var agr5 int = 123
 	fmt.Println(int(age4) + agr5)
+
+	var i int8 = 0
+	var sum = 0
+	for ; i < 6; i++ {
+		sum += int(i)
+	}
+	fmt.Println(sum)
+	fmt.Println("-----------")
+	//元组交换位置
+	a, b = 1, 2
+	fmt.Println(a, b)
+	b, a = a, b
+	fmt.Println(a, b)
+
+	//for  {
+	//	fmt.Println("-----------")
+	//}
+	fmt.Println("-----------")
+	var str = "你好你好abcd"
+	for i, value := range str {
+		var s1 = fmt.Sprintf("%d", i)
+		var s2 = fmt.Sprintf("%c", value)
+		fmt.Println(s1, s2)
+	}
 }
 
 func repalce(a int, b int) (a1 int, b1 int) {
