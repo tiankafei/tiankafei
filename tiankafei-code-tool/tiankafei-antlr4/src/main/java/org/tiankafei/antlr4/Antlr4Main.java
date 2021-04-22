f52d3911d6713d1759d20c359d3db493eda62a12
tiankafei <weishuangshuang89@163.com>
2021-04-22 23:36:34 +0800

    提交“提交antlr4的demo”

--- tiankafei-code-tool/tiankafei-antlr4/src/main/java/org/tiankafei/antlr4/Antlr4Main.java
+++ tiankafei-code-tool/tiankafei-antlr4/src/main/java/org/tiankafei/antlr4/Antlr4Main.java
@@ -3,10 +3,8 @@ package org.tiankafei.antlr4;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.tiankafei.antlr4.expression.RuleSetBaseVisitor;
 import org.tiankafei.antlr4.hello.HelloLexer;
 import org.tiankafei.antlr4.hello.HelloParser;
-import org.tiankafei.antlr4.math.MathBaseVisitor;
 import org.tiankafei.antlr4.math.MathLexer;
 import org.tiankafei.antlr4.math.MathParser;
 
@@ -19,6 +17,7 @@ public class Antlr4Main {
 
     public static void main(String[] args) {
         helloAntlr4();
+        System.out.println("=================");
         mathAntlr4();
 
     }
@@ -27,25 +26,30 @@ public class Antlr4Main {
         HelloLexer lexer = new HelloLexer(new ANTLRInputStream("hello world"));
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        int numberOfOnChannelTokens = tokens.getNumberOfOnChannelTokens();
+        for (int index = 0; index < numberOfOnChannelTokens; index++) {
+            System.out.println(tokens.get(index));
+        }
+
         HelloParser parser = new HelloParser(tokens);
         ParseTree tree = parser.r();
         System.out.println(tree);
-
-        RuleSetBaseVisitor visitor = new RuleSetBaseVisitor();
-        System.out.println(visitor.visit(tree));
-
     }
 
     private static void mathAntlr4(){
-        MathLexer lexer = new MathLexer(new ANTLRInputStream("1+2"));
+        MathLexer lexer = new MathLexer(new ANTLRInputStream("1+(2-1)+4"));
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        int numberOfOnChannelTokens = tokens.getNumberOfOnChannelTokens();
+        for (int index = 0; index < numberOfOnChannelTokens; index++) {
+            System.out.println(tokens.get(index));
+        }
+
         MathParser parser = new MathParser(tokens);
         ParseTree tree = parser.expr();
         System.out.println(tree);
-
-        MathBaseVisitor visitor = new MathBaseVisitor();
-        System.out.println(visitor.visit(tree));
     }
 
 }
