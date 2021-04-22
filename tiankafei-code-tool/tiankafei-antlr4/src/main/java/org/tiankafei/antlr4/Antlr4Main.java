44d0d376a0163d292c0829eef76235faa53cfe91
tiankafei <weishuangshuang89@163.com>
2021-04-22 23:26:22 +0800

    提交“提交antlr4的demo”

--- tiankafei-code-tool/tiankafei-antlr4/src/main/java/org/tiankafei/antlr4/Antlr4Main.java
+++ tiankafei-code-tool/tiankafei-antlr4/src/main/java/org/tiankafei/antlr4/Antlr4Main.java
@@ -2,8 +2,13 @@ package org.tiankafei.antlr4;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.tiankafei.antlr4.expression.RuleSetBaseVisitor;
 import org.tiankafei.antlr4.hello.HelloLexer;
 import org.tiankafei.antlr4.hello.HelloParser;
+import org.tiankafei.antlr4.math.MathBaseVisitor;
+import org.tiankafei.antlr4.math.MathLexer;
+import org.tiankafei.antlr4.math.MathParser;
 
 /**
  * @Author 魏双双
@@ -14,7 +19,7 @@ public class Antlr4Main {
 
     public static void main(String[] args) {
         helloAntlr4();
-
+        mathAntlr4();
 
     }
 
@@ -23,8 +28,24 @@ public class Antlr4Main {
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         HelloParser parser = new HelloParser(tokens);
-        HelloParser.RContext r = parser.r();
-        System.out.println(r);
+        ParseTree tree = parser.r();
+        System.out.println(tree);
+
+        RuleSetBaseVisitor visitor = new RuleSetBaseVisitor();
+        System.out.println(visitor.visit(tree));
+
+    }
+
+    private static void mathAntlr4(){
+        MathLexer lexer = new MathLexer(new ANTLRInputStream("1+2"));
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MathParser parser = new MathParser(tokens);
+        ParseTree tree = parser.expr();
+        System.out.println(tree);
+
+        MathBaseVisitor visitor = new MathBaseVisitor();
+        System.out.println(visitor.visit(tree));
     }
 
 }
