f63ef8c5c6a31745a718abcd6d6d66394c5cf4be
tiankafei <weishuangshuang89@163.com>
2021-05-30 01:34:24 +0800

    提交“更新配置”

---
+++ tiankafei-code-product/tiankafei-service/tiankafei-rule-service/tiankafei-rule-server/src/main/java/org/tiankafei/rule/RuleApplication.java
@@ -0,0 +1,25 @@
+package org.tiankafei.rule;
+
+import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author tiankafei
+ * @since 1.0
+ **/
+@SpringCloudApplication
+@MapperScan(basePackages = {"org.tiankafei.**.mapper"})
+@EnableFeignClients(basePackages = {"org.tiankafei", "com.ruoyi"})
+@EnableHystrixDashboard
+@EnableCustomSwagger2
+public class RuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuleApplication.class, args);
+    }
+
+}
