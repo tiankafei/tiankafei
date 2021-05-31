41a346dacd7205a5f5b74db26b93417cad7f1091
tiankafei <weishuangshuang89@163.com>
2021-05-31 21:54:28 +0800

    提交“更新审核数据对象”

--- tiankafei-code-product/tiankafei-service/tiankafei-rule-common/src/main/java/org/tiankafei/rule/ruledtos/RuleExpressionDto.java
+++ tiankafei-code-product/tiankafei-service/tiankafei-rule-common/src/main/java/org/tiankafei/rule/ruledtos/RuleExpressionDto.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -15,13 +16,28 @@ import lombok.experimental.Accessors;
 @ApiModel(value = "规则表达式 对象", description = "规则表达式 对象")
 public class RuleExpressionDto {
 
+    @ApiModelProperty(value = "规则id")
+    private String ruleId;
+
+    @ApiModelProperty(value = "当前规则表达式")
+    private String expression;
+
+    @ApiModelProperty(value = "替换后的规则表达式")
+    private String newExpression;
+
     @ApiModelProperty(value = "当前规则表达式元素的别名集合")
     private List<String> aliasNameList;
 
     @ApiModelProperty(value = "当前规则表达式元素的别名映射集合")
     private Map<String, ElementMapDto> aliasNameMap;
 
-    @ApiModelProperty(value = "当前规则表达式")
-    private String expression;
+    @ApiModelProperty(value = "当前规则表达式中的行列表达式集合")
+    private List<RankRangeDto> rankRangeDtoList;
+
+    @ApiModelProperty(value = "规则执行类型：1全是一维，2普通二维，3包含聚合函数，4全是常量")
+    private String ruleExecuteType;
+
+    @ApiModelProperty(value = "每一个数据集中的数据唯一标识:key(datasetUniqueIdentifier)——>value(dataUniqueIdentifier)")
+    private Map<String, Set<String>> datasetUniqueIdentifierSetMap;
 
 }
